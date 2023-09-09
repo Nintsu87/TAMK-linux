6d9da8f6361accf8baca667d6ff8f887d0c90b19
student <student@23s-linux49.student.titeweb>
2023-09-09 12:38:40 +0300

    Practice2 task2

---
+++ practice2/task2/helloWorld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	int num = 2;
+	cout << "Hello, World!" << endl;
+	if (num >= 3) {
+		cout << "Oink!" << endl;
+	} else {
+		cout << "Poink!" << endl;
+	}
+	return 0;
+}
